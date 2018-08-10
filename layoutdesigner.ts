967446bba3494bf99bc874ea498d34126551467b
shadowfax6894 <anniekelly6894@gmail.com>
2018-08-10 16:18:52 -0600

    added layoutdesigner to pxtjson

--- layoutdesigner.ts
+++ layoutdesigner.ts
@@ -1,3 +1,14 @@
-
+/**
+ * Create layout for strip of neopixels
+ */
+//% weight=5 color=#2699BF icon="\uf110"
 namespace layoutdesigner {
+    /**
+     * 
+     */
+    //% blockId="stub" block="stub" 
+    //% weight=85 blockGap=8
+    export function stub(): void {
+        //stub
+    }
 }
